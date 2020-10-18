55407445c15f4de2cf0341c21f8c6c2e43731040
dmarmijosa <daniel2667@hotmail.es>
2020-10-17 23:44:44 -0500

    Segundo commit

--- typescript/fundamentos-de-promesas.ts
+++ typescript/fundamentos-de-promesas.ts
@@ -1,15 +1,22 @@
-"use strict";
-(() => {
+(()=>{
+    
+
     console.log('Inicio');
-    const prom1 = new Promise((resolve, reject) => {
+
+    const prom1 = new Promise((resolve, reject)=>{
         setTimeout(() => {
             reject('Se termino el timeout');
         }, 1000);
+
     });
     //then ejecutara cuando todo sale bien
     //catch si todo sale mal y no para el codigo
     prom1
-        .then(mensaje => { console.log(mensaje); })
-        .catch(err => { console.warn(err); });
+        .then(mensaje=>{console.log(mensaje);})
+        .catch(err=>{console.warn(err);})
+    
+
     console.log('Final');
 })();
+
+
