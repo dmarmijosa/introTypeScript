b846494d362a86bfce4d9b00dc85edc23ed8b881
dmarmijosa <daniel2667@hotmail.es>
2020-10-21 17:58:12 -0500

    6to commit

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
 "use strict";
 (() => {
+    /* class Avenger{
+        
+        nombre:string;
+        equipo: string;
+        nombreReal:string;
+        
+        puedePelear:Boolean;
+        peleasGanadas:number
+        
+        constructor(nombr: string,e:string,equipo:string,nombreReal:string,puedePelear:boolean,peleasGanadas:number){
+            this.nombre = nombre
+            
+        }
+    } */
+    class Avenger {
+        /* nombre:string;
+        equipo: string;
+        nombreReal:string;
+        
+        puedePelear:Boolean;
+        peleasGanadas:number */
+        constructor(nombre, equipo, nombreReal, puedePelear = true, peleasGanadas = 0) {
+            this.nombre = nombre;
+            this.equipo = equipo;
+            this.nombreReal = nombreReal;
+            this.puedePelear = puedePelear;
+            this.peleasGanadas = peleasGanadas;
+        }
+    }
+    //objeto
+    const antman = new Avenger('Ant-man', 'Capi');
+    console.log(antman);
 })();
